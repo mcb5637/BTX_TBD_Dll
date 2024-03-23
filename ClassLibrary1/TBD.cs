bce6e97f8a847d49d836ca7e2f18eb938b1baf45
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2024-03-23 15:31:38 +0100

    Add project files.

---
+++ ClassLibrary1/TBD.cs
@@ -0,0 +1,33 @@
+﻿using BattleTech;
+using Harmony;
+using HBS.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBD
+{
+    public class TBD
+    {
+        public static void Init(string directory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("com.github.mcb5637.TBD");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+    [HarmonyPatch(typeof(StarSystem), nameof(StarSystem.Rehydrate))]
+    public class StarSystem_Rehydrate
+    {
+        public static void Postfix(StarSystem __instance)
+        {
+            if (__instance.Tags.Contains("planet_ruins"))
+            {
+                __instance.Tags.Remove("planet_ruins");
+                __instance.Tags.Add("planet_other_ruins");
+            }
+        }
+    }
+}
